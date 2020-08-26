5db52f0256a7c7c8781b668352db37f3f9dc83e5
VGAchuthan <vgachu19@gmil.com>
2020-08-26 21:37:45 +0530

    initial commit
    Git exercise 01 added

---
+++ Exercise01/Git_Ex_01.js
@@ -0,0 +1,39 @@
+var num1, num2, result;
+
+
+
+/*  Function for adding two numbers, add button click event*/
+document.getElementById("btn_add").onclick= function(){
+	num1=document.getElementById("num1").value;
+	num2=document.getElementById("num2").value;
+	document.getElementById("result").value = Number.parseInt(num1) + Number.parseInt(num2);
+	
+}
+/*  Function for subtracting two numbers, sub button click event*/
+document.getElementById("btn_sub").onclick= function(){
+	num1=document.getElementById("num1").value;
+	num2=document.getElementById("num2").value;
+	document.getElementById("result").value = Number.parseInt(num1) - Number.parseInt(num2);
+	
+}
+/*  Function for multiplying two numbers, mul button click event*/
+document.getElementById("btn_mul").onclick= function(){
+	num1=document.getElementById("num1").value;
+	num2=document.getElementById("num2").value;
+	document.getElementById("result").value = Number.parseInt(num1) * Number.parseInt(num2);
+	
+}
+/*  Function for dividing two numbers, div button click event*/
+document.getElementById("btn_div").onclick= function(){
+	num1=document.getElementById("num1").value;
+	num2=document.getElementById("num2").value;
+	document.getElementById("result").value = Number.parseInt(num1) / Number.parseInt(num2);
+	
+}
+/*  Function for resetting all text fields, reset all button click event*/
+document.getElementById("btn_reset").onclick= function(){
+	document.getElementById("num1").value = "";
+	document.getElementById("num2").value = "";
+	document.getElementById("result").value = "";
+	
+}
